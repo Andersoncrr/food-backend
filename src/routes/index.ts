92fe1907073eaeb1ab29018f7ee20d18a2dc8b10
Yicel Gutierrez <yicelgutierrezguillen@gmnail.com>
2024-04-15 22:38:47 -0500

    solved routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,10 @@
 import { Router } from "express";
-import { usersRoutes } from "./usersRoutes";
+import { userRoutes } from "./userRoutes";
 import { employeeRoutes } from "./employeeRoutes";
 import { menuCategoryRoutes } from "./menuCategoryRoutes";
 
 export const router: Router = Router();
 
-router.use("/user", usersRoutes);
 router.use("/employee", employeeRoutes);
-
-
-
 router.use("/user", userRoutes);
 router.use("/menu-category", menuCategoryRoutes);
