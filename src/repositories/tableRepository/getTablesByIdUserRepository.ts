5cf1003d38777246d7ce495ee76c67d1baef4f80
Yicel Gutierrez <yicelgutierrezguillen@gmnail.com>
2024-05-20 22:51:33 -0500

    status deleted

--- src/repositories/tableRepository/getTablesByIdUserRepository.ts
+++ src/repositories/tableRepository/getTablesByIdUserRepository.ts
@@ -1,6 +1,7 @@
+import { STATUS } from '@/const/status';
 import { Table } from '@/models/table';
 
 export const getTablesByIdUserRepository = async (idUser) => {
-    const tables = await Table.find({ idUser });
+    const tables = await Table.find({ idUser, status: { $ne: STATUS.DELETE } });
     return tables;
 };
